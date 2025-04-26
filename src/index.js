8cd01c34fcf63758c72e158aef9a78f8d505bd5f
Starina-Lokich <starinalokich@gmail.com>
2025-04-26 10:25:27 +0300

    feat: added entry point and HTML template
    - Created `src/index.js` file as entry point for the application.
    - Added `src/index.html` file with basic HTML template.

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// src/index.js
+
+import orderByProps from './orderByProps.js';
+import extractSpecial from './destructuring.js';
+
+// Пример использования orderByProps
+const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
+const orderKeys = ['name', 'level'];
+console.log(orderByProps(obj, orderKeys));
+
+// Пример использования extractSpecial
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+  ],
+};
+
+console.log(extractSpecial(character));
